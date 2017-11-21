4f4085a120795ecf05d3d3aac9b2af5f7a5066e2
Aluno <andre_osmarin@hotmail.com>
2017-11-21 02:23:18 -0200

    Project Final

--- app/src/main/java/com/example/aluno/appgenerico/MainActivity.java
+++ app/src/main/java/com/example/aluno/appgenerico/MainActivity.java
@@ -5,17 +5,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.aluno.appgenerico.Modelo.Usuario;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button login;
     private TextView cadastrar;
     private TextView sobre;
+    private EditText txtlogin;
+    private EditText txtsenha;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtlogin = (EditText) findViewById(R.id.editLoginP);
+        txtsenha= (EditText) findViewById(R.id.editSenhaP);
+
+        List<Usuario> usarios = Usuario.listAll(Usuario.class);
+
+        try {
+            for (Usuario u: usarios){
+                Toast.makeText(this, u.getNome(), Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e ){
+            Toast.makeText(this, e.getMessage().toString(), Toast.LENGTH_LONG).show();
+        }
+
+        login = (Button) findViewById(R.id.btnLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    List<Usuario> u = Usuario.find(Usuario.class, "login = ? and senha = ?", txtlogin.getText().toString() , txtsenha.getText().toString());
+                    if (u.size() > 0) {
+                        Toast.makeText(MainActivity.this, "usuário encontrado", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Este usuário não existe!", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e){
+                    Toast.makeText(MainActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();;
+                }
+            }
+        });
 
         cadastrar = (TextView) findViewById(R.id.textCadastrar);
         cadastrar.setOnClickListener(new View.OnClickListener(){
--- app/src/main/java/com/example/aluno/appgenerico/TelaCadastrarActivity.java
+++ app/src/main/java/com/example/aluno/appgenerico/TelaCadastrarActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.aluno.appgenerico.Modelo.Usuario;
+
 public class TelaCadastrarActivity extends AppCompatActivity {
 
     private EditText nome;
@@ -23,7 +25,7 @@ public class TelaCadastrarActivity extends AppCompatActivity {
 
         nome = (EditText) findViewById(R.id.editNome);
         email = (EditText) findViewById(R.id.editEmail);
-        login = (EditText) findViewById(R.id.editLogin);
+        login = (EditText) findViewById(R.id.editUsuario);
         senha = (EditText) findViewById(R.id.editSenha);
         cadastrar = (Button) findViewById(R.id.btnCadastrar);
         repetirsenha = (EditText) findViewById(R.id.editRpSenha);
@@ -33,27 +35,34 @@ public class TelaCadastrarActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (nome.getText().toString().equals("")){
                     Toast.makeText(TelaCadastrarActivity.this, "Preencha o campo nome!", Toast.LENGTH_SHORT).show();
-                    return;
+
                 } else if(email.getText().toString().equals("")) {
                     Toast.makeText(TelaCadastrarActivity.this, "Preencha o campo email!", Toast.LENGTH_SHORT).show();
-                    return;
-                } else if(email.getText().toString().equals("")) {
-                    Toast.makeText(TelaCadastrarActivity.this, "Preencha o campo email!", Toast.LENGTH_SHORT).show();
-                    return;
-                } else if(email.getText().toString().equals("")) {
-                    Toast.makeText(TelaCadastrarActivity.this, "Preencha o campo email!", Toast.LENGTH_SHORT).show();
-                    return;
-                } else if(email.getText().toString().equals("")) {
-                    Toast.makeText(TelaCadastrarActivity.this, "Preencha o campo email!", Toast.LENGTH_SHORT).show();
-                    return;
-                } else if(email.getText().toString().equals("")) {
-                    Toast.makeText(TelaCadastrarActivity.this, "Preencha o campo email!", Toast.LENGTH_SHORT).show();
-                    return;
+
+                } else if(login.getText().toString().equals("")) {
+                    Toast.makeText(TelaCadastrarActivity.this, "Preencha o campo usuário!", Toast.LENGTH_SHORT).show();
+
+                } else if(senha.getText().toString().equals("")) {
+                    Toast.makeText(TelaCadastrarActivity.this, "Preencha o campo senha!", Toast.LENGTH_SHORT).show();
+
+                } else if(repetirsenha.getText().toString().equals("")) {
+                    Toast.makeText(TelaCadastrarActivity.this, "Preencha o campo repetir senha!", Toast.LENGTH_SHORT).show();
+
+                } else if(senha.getText().toString().equals(repetirsenha.getText().toString())){
+                    Usuario usuario = new Usuario();
+                    usuario.setNome(nome.getText().toString());
+                    usuario.setEmail(email.getText().toString());
+                    usuario.setLogin(login.getText().toString());
+                    usuario.setSenha(senha.getText().toString());
+                    usuario.save();
+
+                    Toast.makeText(TelaCadastrarActivity.this, "Foi cadastrado!", Toast.LENGTH_SHORT).show();
+                    nome.setText("");
+                    email.setText("");
+                    login.setText("");
+                    senha.setText("");
                 }
             }
         });
-
-
-        // verificar todos
     }
 }
